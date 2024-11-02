913c1474ad68134c684b008ed8f4282ef35acc13
IhsanMaul56 <sansgrids5601@gmail.com>
2024-11-02 10:28:38 +0700

    Add feature show and udate

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -2,24 +2,92 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
-    /**
-     * index
-     *
-     * @return void
-     */
     public function index()
     {
-        //get all posts
+        //get data product
         $product = Product::where('is_delete', 0)->get();
 
-        //return collection of posts as a resource
+        //return collection of product as a resource
         return new ProductResource(true, 'List Data Product', $product);
+    }
+
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'code'          => 'required',
+            'name'          => 'required',
+            'description'   => 'required',
+            'stock'         => 'required',
+            'price'         => 'required',
+            'category'      => 'required'
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //create product
+        $product = Product::create([
+            'code'          => $request->code,
+            'name'          => $request->name,
+            'description'   => $request->description,
+            'stock'         => $request->stock,
+            'price'         => $request->price,
+            'category'      => $request->category
+        ]);
+
+        //return response
+        return new ProductResource(true, 'Data Product Berhasil Ditambahkan!', $product);
+    }
+
+    public function show($id)
+    {
+        //find product by ID
+        $product = Product::find($id);
+
+        //return single product as a resource
+        return new ProductResource(true, 'Detail Data Product!', $product);
+    }
+
+    public function update(Request $request, $id)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'name'          => 'required',
+            'description'   => 'required',
+            'stock'         => 'required',
+            'price'         => 'required',
+            'category'      => 'required'
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //find post by ID
+        $product = Product::find($id);
+
+        // uppdate product
+        $product->update([
+            'name'          => $request->name,
+            'description'   => $request->description,
+            'stock'         => $request->stock,
+            'price'         => $request->price,
+            'category'      => $request->category
+        ]);
+
+        //return response
+        return new ProductResource(true, 'Data Post Berhasil Diubah!', $product);
     }
 }
